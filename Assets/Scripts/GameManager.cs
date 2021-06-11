f554c4f244de8fb95b091d6e06aae87404e41512
Sahil <sahiltandon93@gmail.com>
2021-06-11 16:09:06 -0400

    Added Gamemanager stub

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : Singleton<GameManager>
+{
+    public GameObject player;
+    public enum GameStates{TitleScreen, Paused, InGame };
+    public GameStates currentState = GameStates.TitleScreen;
+
+    public delegate void GameStartHandler();
+    public event GameStartHandler GameStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
