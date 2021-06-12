b31958d30a217fc5aef5d64aa303e6a6cd12b182
Sahil <sahiltandon93@gmail.com>
2021-06-12 10:54:46 -0400

    fixed the string distance thing

--- Assets/Scripts/PinataString.cs
+++ Assets/Scripts/PinataString.cs
@@ -23,9 +23,7 @@ public class PinataString : MonoBehaviour
     {
         ropeDistance = Vector2.Distance(connectionPoint.position, transform.position);
         //god this is a mess. Basically sets the string length to the equivalent point between its min & max range to match the distance's current point between its min & max range
-        pinataLength = Mathf.Lerp(stringLengthRange.x, stringLengthRange.y, Mathf.InverseLerp(playerDistanceRange.x, playerDistanceRange.y, Mathf.Clamp(ropeDistance, playerDistanceRange.x, playerDistanceRange.y))); 
+        pinataLength = Mathf.Lerp(stringLengthRange.y, stringLengthRange.x, Mathf.InverseLerp(playerDistanceRange.x, playerDistanceRange.y, Mathf.Clamp(ropeDistance, playerDistanceRange.x, playerDistanceRange.y))); 
         pinataString.distance = pinataLength;
     }
-
-
 }
