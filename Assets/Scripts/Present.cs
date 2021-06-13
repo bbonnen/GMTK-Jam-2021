8854f022e9f0f460c06b4d193f1711c37a11a756
Sahil <sahiltandon93@gmail.com>
2021-06-12 23:47:25 -0400

    present progress bar

--- Assets/Scripts/Present.cs
+++ Assets/Scripts/Present.cs
@@ -9,6 +9,10 @@ public class Present : MonoBehaviour
     public bool isWrapping { get; private set; }
     public bool isWrapped { get; private set; }
 
+    public Transform statusBar;
+    public GameObject statusBarParent;
+    private float wrappingTimer = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,12 +20,17 @@ public class Present : MonoBehaviour
         isWrapped = false;
         cancelFlag = false;
         wrappingTime = 2.0f;
+        statusBarParent.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isWrapping && !isWrapped)
+        {
+            wrappingTimer += Time.deltaTime;
+            statusBar.localScale = new Vector3(Mathf.Lerp(0, 1, wrappingTimer / wrappingTime), statusBar.localScale.y, statusBar.localScale.z);
+        }
     }
 
     public void StartWrapping()
@@ -29,6 +38,8 @@ public class Present : MonoBehaviour
         if (!isWrapped && !isWrapping)
         {
             isWrapping = true;
+            wrappingTimer = 0;
+            statusBarParent.SetActive(true);
             //start timed event in which at the end, you check the cancel flag and then complete
             StartCoroutine(FinishWrappingOnDelay(wrappingTime));
 
